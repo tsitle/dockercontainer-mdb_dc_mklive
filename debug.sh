3ac07cffc458287d117a820f15682c26e0b74fbe
Thomas <technisandk@gmail.com>
2019-04-30 15:52:56 +0200

    Improve multi CPU arch support

--- debug.sh
+++ debug.sh
@@ -48,17 +48,33 @@ VAR_MYDIR="$(dirname "$VAR_MYDIR")"
 
 # Outputs CPU architecture string
 #
+# @param string $1 debian_rootfs|debian_dist
+#
 # @return int EXITCODE
 function _getCpuArch() {
 	case "$(uname -m)" in
 		x86_64*)
-			echo -n "x86_64"
+			echo -n "amd64"
 			;;
-		aarch64)
-			echo -n "arm_64"
+		aarch64*)
+			if [ "$1" = "debian_rootfs" ]; then
+				echo -n "arm64v8"
+			elif [ "$1" = "debian_dist" ]; then
+				echo -n "arm64"
+			else
+				echo "$VAR_MYNAME: Error: invalid arg '$1'" >/dev/stderr
+				return 1
+			fi
 			;;
 		armv7*)
-			echo -n "arm_32"
+			if [ "$1" = "debian_rootfs" ]; then
+				echo -n "arm32v7"
+			elif [ "$1" = "debian_dist" ]; then
+				echo -n "armhf"
+			else
+				echo "$VAR_MYNAME: Error: invalid arg '$1'" >/dev/stderr
+				return 1
+			fi
 			;;
 		*)
 			echo "$VAR_MYNAME: Error: Unknown CPU architecture '$(uname -m)'" >/dev/stderr
@@ -68,14 +84,14 @@ function _getCpuArch() {
 	return 0
 }
 
-_getCpuArch >/dev/null || exit 1
+_getCpuArch debian_dist >/dev/null || exit 1
 
 # ----------------------------------------------------------
 
 cd "$VAR_MYDIR" || exit 1
 
 LVAR_REPO_PREFIX="tsle"
-LVAR_IMAGE_NAME="mdb-mklive-$(_getCpuArch)"
+LVAR_IMAGE_NAME="mdb-mklive-$(_getCpuArch debian_dist)"
 LVAR_IMAGE_VER="1.13"
 
 # ----------------------------------------------------------
